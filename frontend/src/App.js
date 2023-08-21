bdae8482e018d26816b7cde7b9e07c20f191c0fe
강경규 <qwzx16@naver.com>
2023-08-21 13:13:21 +0900

    feat: 로그인, 회원가입 폼 및 정규식 로직작성 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import Login from "./pages/Login";
-import Join from "./pages/Join";
+import SignUp from "./pages/SignUp";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const inputValue = (e, setValue) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+  const handleFocus = (value, setValue) => {
+    value ? setValue(false) : setValue(true);
+  };
+  const inputClear = (e, setValue) => {
+    e.preventDefault();
+    setValue("");
+  };
   return (
     <div className="App">
       <Routes>
@@ -11,15 +22,23 @@ function App() {
           path="/login"
           element={
             <>
-              <Login></Login>
+              <Login
+                inputValue={inputValue}
+                handleFocus={handleFocus}
+                inputClear={inputClear}
+              ></Login>
             </>
           }
         ></Route>
         <Route
-          path="/join"
+          path="/signup"
           element={
             <>
-              <Join></Join>
+              <SignUp
+                inputValue={inputValue}
+                handleFocus={handleFocus}
+                inputClear={inputClear}
+              ></SignUp>
             </>
           }
         ></Route>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Login.module.css";
+import styles from "./Auth.module.css";
 import { Link } from "react-router-dom";
 
-export default function Login() {
+export default function Login({ inputValue, handleFocus, inputClear }) {
   const [email, setEmail] = useState("");
   const [isEmailFocused, setIsEmailFocused] = useState(false);
   const [password, setPassword] = useState("");
   const [isPassWordFocused, setIsPassWordFocused] = useState(false);
   const [isButtonActive, setIsButtonActive] = useState(false);
 
-  const handleFocus = (value, setValue) => {
-    value ? setValue(false) : setValue(true);
-  };
-  const inputValue = (e, setValue) => {
+  const FormSubmit = (e) => {
     e.preventDefault();
-    setValue(e.target.value);
-  };
-  const loginFormSubmit = (e) => {
-    e.preventDefault();
+    console.log(`Email : ${email}\npassword : ${password}`);
   };
 
   useEffect(() => {
@@ -25,76 +19,112 @@ export default function Login() {
   });
   return (
     <>
-      <div className={`${styles.loginBackGround}`}>
-        <div className={`${styles.loginContainer}`}>
-          <div className={`${styles.loginFormHeader}`}>
+      <div
+        className={`${styles.BackGround} flex flex-col justify-center items-center w-screen h-screen`}
+      >
+        <div
+          className={`${styles.Container} flex flex-col justify-center items-center rounded-lg`}
+        >
+          <div className={`${styles.FormHeader} mt-10 text-center font-black`}>
             <h1>HONG CHA</h1>
             <p>동시방영 신작부터 역대 인기작까지</p>
             <p>한 곳에서 편안-하게!</p>
           </div>
-          <form className={`${styles.loginForm}`} action="" method="POST">
+          <form className={`${styles.Form}`} action="" method="POST">
             <div
-              className={`${styles.loginFormItem} ${
-                isEmailFocused ? styles.loginFormItemFocus : ""
-              }`}
+              className={`${styles.FormItem} ${
+                isEmailFocused ? styles.FormItemFocus : ""
+              } flex flex-col w-80`}
             >
-              <label className={`${styles.loginFormLabel}`} htmlFor="">
+              <label className={`${styles.FormLabel}`} htmlFor="">
                 이메일
               </label>
-              <input
-                className={`${styles.loginFormInput}`}
-                placeholder="이메일 입력해주세요"
-                type="email"
-                value={email || ""}
-                onChange={(e) => {
-                  inputValue(e, setEmail);
-                }}
-                onFocus={() => {
-                  handleFocus(isEmailFocused, setIsEmailFocused);
-                }}
-                onBlur={() => {
-                  handleFocus(isEmailFocused, setIsEmailFocused);
-                }}
-              />
+              <div className={`relative`}>
+                <input
+                  className={`${styles.FormInput}`}
+                  placeholder="이메일 입력해주세요"
+                  type="Email"
+                  value={email || ""}
+                  onChange={(e) => {
+                    inputValue(e, setEmail);
+                  }}
+                  onFocus={() => {
+                    handleFocus(isEmailFocused, setIsEmailFocused);
+                  }}
+                  onBlur={() => {
+                    handleFocus(isEmailFocused, setIsEmailFocused);
+                  }}
+                />
+                {isEmailFocused ? (
+                  <button
+                    className={`${styles.InputButton} flex items-center rounded-full font-black absolute top-0 right-0`}
+                    onMouseDown={(e) => {
+                      inputClear(e, setEmail);
+                    }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                    }}
+                  >
+                    X
+                  </button>
+                ) : null}
+              </div>
             </div>
             <div
-              className={`${styles.loginFormItem} ${
-                isPassWordFocused ? styles.loginFormItemFocus : ""
-              }`}
+              className={`${styles.FormItem} ${
+                isPassWordFocused ? styles.FormItemFocus : ""
+              } relative`}
             >
-              <label className={`${styles.loginFormLabel}`} htmlFor="">
+              <label className={`${styles.FormLabel}`} htmlFor="">
                 비밀번호
               </label>
-              <input
-                className={`${styles.loginFormInput}`}
-                placeholder="비밀번호를 입력해주세요"
-                type="password"
-                value={password || ""}
-                onChange={(e) => {
-                  inputValue(e, setPassword);
-                }}
-                onFocus={() => {
-                  handleFocus(isPassWordFocused, setIsPassWordFocused);
-                }}
-                onBlur={() => {
-                  handleFocus(isPassWordFocused, setIsPassWordFocused);
-                }}
-              />
+              <div className={`relative`}>
+                <input
+                  className={`${styles.FormInput}`}
+                  placeholder="비밀번호를 입력해주세요"
+                  type="password"
+                  value={password || ""}
+                  onChange={(e) => {
+                    inputValue(e, setPassword);
+                  }}
+                  onFocus={() => {
+                    handleFocus(isPassWordFocused, setIsPassWordFocused);
+                  }}
+                  onBlur={() => {
+                    handleFocus(isPassWordFocused, setIsPassWordFocused);
+                  }}
+                />
+                {isPassWordFocused ? (
+                  <button
+                    className={`${styles.InputButton} flex items-center rounded-full font-black absolute top-0 right-0`}
+                    onMouseDown={(e) => {
+                      inputClear(e, setPassword);
+                    }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                    }}
+                  >
+                    X
+                  </button>
+                ) : null}
+              </div>
             </div>
-            <p className={`${styles.loginFormText}`}>
-              계정이 없으시다면..<Link to={"/join"}> 가입하기</Link>
+            <p className={`${styles.FormText} mt-2`}>
+              계정이 없으시다면..<Link to={"/"}> 가입하기</Link>
             </p>
-            <button
-              className={`${styles.loginFormBtn} ${
-                isButtonActive ? styles.loginFormBtnCompletion : ""
-              }`}
-              disabled={isButtonActive}
-              onClick={(e) => {
-                loginFormSubmit(e);
-              }}
-            >
-              로그인
-            </button>
+            <div className="mb-10">
+              <button
+                className={`${styles.FormBtn} ${
+                  isButtonActive ? styles.FormBtnCompletion : ""
+                } flex items-center justify-cente w-full`}
+                disabled={!isButtonActive}
+                onClick={(e) => {
+                  FormSubmit(e);
+                }}
+              >
+                로그인
+              </button>
+            </div>
           </form>
         </div>
       </div>
