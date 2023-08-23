fbfc98eaf6b87664b683395a6fa4141128c708d9
강경규 <qwzx16@naver.com>
2023-08-23 23:55:55 +0900

    feat: be/fe merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,6 @@ function App() {
   };
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route
           path="/login"
@@ -44,22 +43,6 @@ function App() {
           }
         ></Route>
       </Routes>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload;;
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> origin
     </div>
   );
 }
